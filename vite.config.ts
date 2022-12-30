26bac518ae9ead9dd83ecaef3e6c026b2b5bbcd3
David Emanuel Luksic <david@laralab.de>
2022-12-30 13:08:22 +0100

    Add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from "vite";
 import devtools from 'solid-devtools/vite'
 import postcssNetsing from 'postcss-nesting'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import vercel from 'solid-start-vercel'
 
 export default defineConfig({
   plugins: [
     devtools({ autoname: true }),
     solid({
       ssr: true,
+      adapter: vercel({ edge: false }),
     }),
     basicSsl(),
   ],
