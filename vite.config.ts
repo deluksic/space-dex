a3f646f264860434c92c545d29eb895bbd9b60d9
David Emanuel Luksic <david@laralab.de>
2022-12-31 03:32:32 +0100

    Remove devtools as a test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import autoprefixer from 'autoprefixer'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import devtools from 'solid-devtools/vite'
+// import devtools from 'solid-devtools/vite'
 import postcssNetsing from 'postcss-nesting'
 import solid from 'solid-start/vite'
 import vercel from 'solid-start-vercel'
 
 export default defineConfig({
   plugins: [
-    devtools({ autoname: true }),
+    // devtools({ autoname: true }),
     solid({
       ssr: true,
       adapter: vercel({ edge: false }),
